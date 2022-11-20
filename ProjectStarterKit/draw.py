65df7f054fa9a11c209accf153626fb9667389da
Cliftan <clifton3833@gmail.com>
2022-11-19 20:42:19 -0800

    s
    s

--- ProjectStarterKit/draw.py
+++ ProjectStarterKit/draw.py
@@ -10,23 +10,22 @@ def isBlack(pixel):
 def recolorImage(img,color):
   imgHeight = len(img)
   imgWidth = len(img[0])  
-  canvas = cmpt120image.getWhiteImage(imgHeight, imgWidth)
+  result = cmpt120image.getWhiteImage(imgHeight, imgWidth)
   imgHeight = len(img)
   imgWidth = len(img[0]) 
   for row in range(imgHeight):
     for col in range(imgWidth):
       if isBlack(img[row][col]):
-        canvas[row][col] = color
-
-  #return canvas
-  cmpt120image.showImage(canvas)
+        result[row][col] = color
+  return result
+  cmpt120image.showImage(result)
   
 def minify(img):
   imgHeight = len(img)
   imgWidth = len(img[0])
   x = (imgHeight/2)
   y = (imgWidth/2)
-  canvas = cmpt120image.getBlackImage(int(x),int(y))
+  result = cmpt120image.getBlackImage(int(x),int(y))
   for row in range(0, imgHeight, 2):
     for col in range(0, imgWidth, 2):
       r = 0
@@ -41,35 +40,34 @@ def minify(img):
       g = g/4
       b = b/4
       list = [r, g, b]
-      canvas[int(row/2)][int(col/2)] = list
-  return canvas
-  #cmpt120image.showImage(canvas)
+      result[int(row/2)][int(col/2)] = list
+  return result
+  #cmpt120image.showImage(result)
 
 
 
 def mirror(img):
   imgHeight = len(img)
   width = len(img[0])   
-  canvas = cmpt120image.getBlackImage(imgHeight,width)
+  result = cmpt120image.getBlackImage(imgHeight,width)
   for row in range(imgHeight): 
     counter = 0
     for col in range(width-1, -1, -1):
-      canvas[row][counter] = (img[row][col])
+      result[row][counter] = (img[row][col])
       counter += 1
-  return canvas
-  #cmpt120image.showImage(canvas)
+  return result
+  #cmpt120image.showImage(result)
  
-def drawItem(img,row,col):
-  canvas = cmpt120image.getWhiteImage(400, 300)
-  imgHeight = len(img)
-  imgWidth = len(img[0]) 
-  canvas[row][col] = img[0][0]
+def drawItem(canvas,item,row,col):
+  imgHeight = len(item)
+  imgWidth = len(item[0]) 
+  canvas[row][col] = item[0][0]
   for i in range(imgHeight):
     for j in range(imgWidth):
       if isBlack(img[i][j]):
         canvas[row+i][col+j] = img[i][j]
-  return canvas
-  #cmpt120image.showImage(canvas)
+  # return result
+  cmpt120image.showImage(canvas)
 
 
 #def distributeItems(canvas,item,n):
@@ -82,5 +80,5 @@ img = cmpt120image.getImage("child.png")
 #minify(img)
 #recolorImage(img,[255,0,0])
 #mirror(img)
-drawItem(img, 100, 0)
+drawItem(cmpt120image.getWhiteImage(400, 300),img, 100, 0)
 input()
