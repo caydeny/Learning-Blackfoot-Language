f385be6833d9fe6de843675e75f0531386ade410
caydeny <caydenyoo@hotmail.com>
2022-11-19 17:54:36 -0800

    Changes + drawItem function completed
    Moved "apples.png" back into "images" folder and complete drawItem function (without canvas in def of drawItem())

--- ProjectStarterKit/draw.py
+++ ProjectStarterKit/draw.py
@@ -1,33 +1,34 @@
 # Your header
 import cmpt120image
 
-def recolorImage(img,color):
-  height = len(img)
-  width = len(img[0])  
-  canvas = cmpt120image.getBlackImage(height,width)
-  def iswhite(pixel):
+def isBlack(pixel):
     r = pixel[0]
     g = pixel[1]
     b = pixel[2]
-    return r == 255  and g == 255 and b == 255
-  height = len(img)
-  width = len(img[0]) 
-  for row in range(height):
-    for col in range(width):
-      if iswhite(img[row][col]):
-        canvas[row][col] = img[row][col]
-      else:
-        canvas[row][col] = color
-  return canvas
+    return r < 100 and g < 100 and b < 100
 
+def recolorImage(img,color):
+  imgHeight = len(img)
+  imgWidth = len(img[0])  
+  canvas = cmpt120image.getWhiteImage(imgHeight, imgWidth)
+  imgHeight = len(img)
+  imgWidth = len(img[0]) 
+  for row in range(imgHeight):
+    for col in range(imgWidth):
+      if isBlack(img[row][col]):
+        canvas[row][col] = color
+
+  #return canvas
+  cmpt120image.showImage(canvas)
+  
 def minify(img):
-  height = len(img)
-  width = len(img[0])
-  x = (height/2)
-  y = (width/2)
+  imgHeight = len(img)
+  imgWidth = len(img[0])
+  x = (imgHeight/2)
+  y = (imgWidth/2)
   canvas = cmpt120image.getBlackImage(int(x),int(y))
-  for row in range(0,height,2):
-    for col in range(0,width,2):
+  for row in range(0, imgHeight, 2):
+    for col in range(0, imgWidth, 2):
       r = 0
       g = 0
       b = 0
@@ -42,28 +43,44 @@ def minify(img):
       list = [r, g, b]
       canvas[int(row/2)][int(col/2)] = list
   return canvas
+  #cmpt120image.showImage(canvas)
 
 
 
 def mirror(img):
-  height = len(img)
+  imgHeight = len(img)
   width = len(img[0])   
-  canvas = cmpt120image.getBlackImage(height,width)
-  for row in range(height): 
+  canvas = cmpt120image.getBlackImage(imgHeight,width)
+  for row in range(imgHeight): 
     counter = 0
     for col in range(width-1, -1, -1):
       canvas[row][counter] = (img[row][col])
       counter += 1
   return canvas
-  
-# def drawItem(img,item,row,col):
-#   # Add your code here
-  
-#def distributeItems(img,item,n):
+  #cmpt120image.showImage(canvas)
+ 
+def drawItem(img,row,col):
+  canvas = cmpt120image.getWhiteImage(400, 300)
+  imgHeight = len(img)
+  imgWidth = len(img[0]) 
+  canvas[row][col] = img[0][0]
+  for i in range(imgHeight):
+    for j in range(imgWidth):
+      if isBlack(img[i][j]):
+        canvas[row+i][col+j] = img[i][j]
+  return canvas
+  #cmpt120image.showImage(canvas)
+
+
+#def distributeItems(canvas,item,n):
 #   # Add your code here
 
-img = cmpt120image.getImage("apples.png")
-minify(img)
-recolorImage(img,[255,0,0])
-mirror(img)
 
+
+img = cmpt120image.getImage("child.png")  
+#img = cmpt120image.getImage("apples.png")
+#minify(img)
+#recolorImage(img,[255,0,0])
+#mirror(img)
+drawItem(img, 100, 0)
+input()
